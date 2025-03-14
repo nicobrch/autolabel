01e1ba624af6d0a2e18aae938401d41fac57734c
nico <nicolas.brch@gmail.com>
2025-03-14 02:01:48 -0300

    ✏️ Update functions to follow new database models

--- server/utils.py
+++ server/utils.py
@@ -9,7 +9,7 @@ import cv2
 from pathlib import Path
 from sqlalchemy.orm import Session
 from db import get_db, create_tables
-from models import Object, Point
+from models import Object, Point, Frame, Mask
 from sam2.build_sam import build_sam2_video_predictor
 
 
@@ -92,7 +92,31 @@ def extract_video_metadata(video_path: str):
     }
 
 
-def extract_frames_at_frame_step(video_path: str, frame_step: int):
+def get_frames_list(frames_dir: str) -> list:
+    """
+    Get a list of frames from the specified directory.
+    Args:
+        frames_dir (str): Path to the directory containing frames.
+    Returns:
+        List of frame file paths.
+    """
+    # Convert to Path object
+    frames_dir = Path(frames_dir)
+
+    # Check if the directory exists
+    if not frames_dir.exists():
+        raise FileNotFoundError(
+            f"Frames directory '{frames_dir}' does not exist.")
+
+    # Get the list of frames
+    frames = sorted(frames_dir.glob("*.jpg"))
+    if not frames:
+        raise FileNotFoundError(
+            f"No frames found in directory '{frames_dir}'.")
+    return frames
+
+
+def extract_frames_at_frame_step(video_path: str, frame_step: int, db: Session = next(get_db())):
     """"
     "Extract frames from a video at a specified frame step using ffmpeg."
     Args:
@@ -100,14 +124,33 @@ def extract_frames_at_frame_step(video_path: str, frame_step: int):
         frame_step (int): Step size for frame extraction. For example, if
                           frame_step is 5, every 5th frame will be extracted.
     """
+    # Check if the video file exists
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video file '{video_path}' not found.")
+
+    # Check if the video exists on the database
+    video = db.query(Object).filter_by(file_path=video_path).first()
+    if not video:
+        raise ValueError(f"Video '{video_path}' not found in database.")
+
     # Create base frames directory if it doesn't exist
-    base_frames_dir = Path("frames")
+    base_frames_dir = Path("data/frames")
     base_frames_dir.mkdir(exist_ok=True)
 
     # Create video frames directory if it doesn't exist
     video_name = Path(video_path).stem
-    video_frames_dir = base_frames_dir / video_name
-    video_frames_dir.mkdir(exist_ok=True)
+    frames_dir = base_frames_dir / video_name / \
+        "original"  # e.g. "data/frames/video_name/original"
+    frames_dir.mkdir(exist_ok=True)
+
+    # Remove existing frames in the directory
+    for file in frames_dir.glob("*.jpg"):
+        file.unlink()
+
+    # Remove existing frames from the database, if any
+    db.query(Frame).filter_by(video_id=video.id).delete()
+    db.commit()
+    db.refresh(video)
 
     try:
         # Prepare ffmpeg command to extract frames
@@ -116,23 +159,39 @@ def extract_frames_at_frame_step(video_path: str, frame_step: int):
             "-i", video_path,
             "-vsync", "vfr",
             "-vf", f"select='not(mod(n,{frame_step}))'",
-            str(video_frames_dir / "%04d.jpg"),
+            str(frames_dir / "%04d.jpg"),
         ]
 
         subprocess.run(cmd, check=True, stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
 
         # Check if frames were extracted
-        if not any(video_frames_dir.glob("*.jpg")):
+        if not any(frames_dir.glob("*.jpg")):
             raise ValueError(
                 f"No frames were extracted from video '{video_path}'.")
 
-        return video_frames_dir
+        # Store frame data in the database
+        frames = get_frames_list(frames_dir)
+        for frame in frames:
+            # Create a new Frame object
+            new_frame = Frame(
+                video_id=video.id,
+                frame_number=int(frame.stem),
+                file_path=str(frame)
+            )
+            db.add(new_frame)
+            db.commit()
+            db.refresh(new_frame)
+
+        print(
+            f"Extracted {len(frames)} frames from '{video_path}' to '{frames_dir}'.")
+
+        return frames_dir
     except (subprocess.SubprocessError, ValueError, KeyError) as e:
         print(f"Error extracting frames: {e}", file=sys.stderr)
 
 
-def create_object(clip_id: int, name: str):
+def create_object(video_id: int, name: str):
     """
     Create a new object in the database.
 
@@ -152,7 +211,7 @@ def create_object(clip_id: int, name: str):
     color = f"#{r:02x}{g:02x}{b:02x}"
 
     new_object = Object(
-        clip_id=clip_id,
+        video_id=video_id,
         name=name,
         color=color,  # Default color, can be changed later
         mask=None
@@ -166,29 +225,7 @@ def create_object(clip_id: int, name: str):
     return new_object
 
 
-def get_frames_list(video_path: str) -> list:
-    # Check if the frames directory exists
-    base_frames_dir = Path("frames")
-    if not base_frames_dir.exists():
-        raise FileNotFoundError(
-            f"Frames directory '{base_frames_dir}' does not exist.")
-
-    # Check if frames directory exists
-    video_name = Path(video_path).stem
-    video_frames_dir = base_frames_dir / video_name
-    if not video_frames_dir.exists():
-        raise FileNotFoundError(
-            f"Frames directory '{video_frames_dir}' does not exist.")
-
-    # Get the list of frames
-    frames = sorted(video_frames_dir.glob("*.jpg"))
-    if not frames:
-        raise FileNotFoundError(
-            f"No frames found in directory '{video_frames_dir}'.")
-    return frames
-
-
-def initialize_sam_predictor(checkpoint):
+def build_sam_predictor(checkpoint: str):
     model_map = {
         "tiny": ("../sam2/checkpoints/sam2.1_hiera_tiny.pt", "//home/nico/autolabel/sam2/sam2/configs/sam2.1/sam2.1_hiera_t.yaml"),
         "small": ("../sam2/checkpoints/sam2.1_hiera_small.pt", "/home/nico/autolabel/sam2/sam2/configs/sam2.1/sam2.1_hiera_s.yaml"),
@@ -214,6 +251,25 @@ def initialize_sam_predictor(checkpoint):
             config_path, checkpoint_path, device=device)
 
 
+def initialize_sam_predictor_state(frames_dir: str, sam_predictor):
+    """
+    Initialize the SAM2 predictor state with the frames.
+
+    Args:
+        frames_dir (str): Path to the directory containing frames.
+        sam_predictor: SAM2 predictor instance.
+    """
+    # Check if the directory exists
+    frames_dir = Path(frames_dir)
+    if not frames_dir.exists():
+        raise FileNotFoundError(
+            f"Frames directory '{frames_dir}' does not exist.")
+
+    # Initialize SAM2 predictor state
+    inference_state = sam_predictor.init_state(video_path=str(frames_dir))
+    sam_predictor.reset_state(inference_state)
+
+
 def serialize_mask(mask):
     return pickle.dumps(mask)
 
@@ -222,56 +278,89 @@ def deserialize_mask(mask_blob):
     return pickle.loads(mask_blob)
 
 
-def add_point_and_segment(x: int, y: int, positive: int, object_id: int, frames_dir: str, sam_predictor, db: Session = next(get_db())):
+def segmentate_object(object_id: int, frame_idx: int, sam_predictor, inference_state, db: Session = next(get_db())):
+    """
+    Segmentate an object using SAM2 and save the mask to the database.
+
+    Args:
+        object_id (int): ID of the object to segment.
+        sam_predictor: SAM2 predictor instance.
+        db (Session, optional): Database session. If None, gets a new session.
+    """
     # Get object from DB
     current_object = db.query(Object).filter_by(id=object_id).first()
     if not current_object:
         raise ValueError(f"Object with ID {object_id} not found in database.")
 
-    # SAM2 state
-    inference_state = sam_predictor.init_state(frames_dir)
-    sam_predictor.reset_state(inference_state)
+    # Get all points for this object
+    points = db.query(Point).filter_by(object_id=object_id).all()
+    if not points:
+        raise ValueError(f"No points found for object ID {object_id}.")
+
+    # Check if the frame exists on the database
+    current_frame = db.query(Frame).filter_by(
+        video_id=current_object.video_id, frame_number=frame_idx).first()
+    if not current_frame:
+        raise ValueError(f"Frame with ID {frame_idx} not found in database.")
+
+    # Prepare data for SAM2
+    points_array = np.array([[p.x, p.y] for p in points], dtype=np.float32)
+    labels_array = np.array([p.label for p in points], dtype=np.int32)
+
+    # Perform segmentation
+    _, _, out_mask_logits = sam_predictor.add_new_points_or_box(
+        inference_state=inference_state,
+        frame_idx=frame_idx,
+        obj_id=object_id,
+        points=points_array,
+        labels=labels_array,
+    )
+
+    # Save the mask to the database as a binary blob
+    mask = (out_mask_logits[0] > 0.0).cpu().numpy().squeeze()
+    mask_blob = serialize_mask(mask)
+    new_mask = Mask(
+        object_id=object_id,
+        frame_id=current_frame.id,
+        mask=mask_blob
+    )
+    db.add(new_mask)
+    db.commit()
+    db.refresh(new_mask)
+    return mask
+
+
+def create_point(x: int, y: int, positive: int, object_id: int, db: Session = next(get_db())):
+    # Get object from DB
+    current_object = db.query(Object).filter_by(id=object_id).first()
+    if not current_object:
+        raise ValueError(f"Object with ID {object_id} not found in database.")
 
     # Append the new point to the existing points
     new_point = Point(
         object_id=object_id,
-        x=x,
-        y=y,
-        label=positive
+        x=x,    # X coordinate of the point
+        y=y,    # Y Coordinate of the point
+        label=positive  # 1 for positive, 0 for negative
     )
     db.add(new_point)
     db.commit()
     db.refresh(new_point)
 
-    # Then retrieve all existing points of the object
-    existing_points = db.query(Point).filter_by(object_id=object_id).all()
 
-    # Prepare data for SAM2
-    points = np.array([[p.x, p.y] for p in existing_points], dtype=np.float32)
-    labels = np.array([p.label for p in existing_points], dtype=np.int32)
+def remove_point(point_id: int, db: Session = next(get_db())):
+    # Get point from DB
+    point = db.query(Point).filter_by(id=point_id).first()
+    if not point:
+        raise ValueError(f"Point with ID {point_id} not found in database.")
 
-    print(f"Points: {points}")
-    print(f"Labels: {labels}")
-
-    # Perform segmentation
-    _, _, out_mask_logits = sam_predictor.add_new_points_or_box(
-        inference_state=inference_state,
-        frame_idx=0,    # Assuming we always work with the first frame
-        obj_id=object_id,
-        points=points,
-        labels=labels,
-    )
-
-    # Save the mask to the database as a binary blob
-    mask = (out_mask_logits[0] > 0.0).cpu().numpy().squeeze()
-
-    mask_blob = serialize_mask(mask)
-    current_object.mask = mask_blob
+    # Delete the point
+    db.delete(point)
     db.commit()
-    db.refresh(current_object)
+    db.refresh(point)
 
 
-def draw_objects_masks(frame_path: str, object_ids: list, output_dir: str, db: Session = next(get_db())):
+def draw_objects_masks_on_frame(frame_path: str, object_ids: list, output_dir: str, db: Session = next(get_db())):
     """
     Draw object masks onto a frame and save as new image.
 
@@ -344,18 +433,9 @@ def draw_objects_masks(frame_path: str, object_ids: list, output_dir: str, db: S
             cv2.circle(overlay, (point.x, point.y), 5, (255, 255, 255), 1)
 
     frame_path_obj = Path(frame_path)
-    output_path = f"{output_dir}/{frame_path_obj.stem}_masked{frame_path_obj.suffix}"
+    output_path = f"{output_dir}/{frame_path_obj.stem}{frame_path_obj.suffix}"
 
     # Save the result
     cv2.imwrite(output_path, overlay)
 
     return output_path
-
-
-if __name__ == "__main__":
-    # Create objects
-    db = next(get_db())
-    frames_dir = "frames/footage_kdxonm"
-    draw_objects_masks(
-        "frames/footage_kdxonm/0001.jpg", [1, 2], "frames/footage_kdxonm_masked")
-    # create_tables()
